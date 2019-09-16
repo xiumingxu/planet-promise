8fef742d274a60e1e7ef67300c5ab30d0e203bbe
XIX <xiuming.x@outlook.com>
2019-09-15 22:52:33 -0700

    for now

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,63 +10,62 @@ Hint: you'll probably still need to use .map.
 /* jshint unused: false */
 
 (function(document) {
-  'use strict';
+	'use strict';
 
-  var home = null;
+	var home = null;
 
-  /**
+	/**
    * Helper function to show the search query.
    * @param {String} query - The search query.
    */
-  function addSearchHeader(query) {
-    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
-  }
+	function addSearchHeader(query) {
+		home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+	}
 
-  /**
+	/**
    * Helper function to create a planet thumbnail.
    * @param  {Object} data - The raw data describing the planet.
    */
-  function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
-  }
+	function createPlanetThumb(data) {
+		var pT = document.createElement('planet-thumb');
+		for (var d in data) {
+			pT[d] = data[d];
+		}
+		home.appendChild(pT);
+	}
 
-  /**
+	/**
    * XHR wrapped in a promise
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
-  function get(url) {
-    return fetch(url);
-  }
+	function get(url) {
+		return fetch(url);
+	}
 
-  /**
+	/**
    * Performs an XHR for a JSON and returns a parsed JSON response.
    * @param  {String} url - The JSON URL to fetch.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
-  function getJSON(url) {
-    return get(url).then(function(response) {
-      return response.json();
-    });
-  }
+	function getJSON(url) {
+		return get(url).then(function(response) {
+			return response.json();
+		});
+	}
 
-  window.addEventListener('WebComponentsReady', function() {
-    home = document.querySelector('section[data-route="home"]');
-    /*
+	window.addEventListener('WebComponentsReady', function() {
+		home = document.querySelector('section[data-route="home"]');
+		/*
     Refactor this code with Promise.all!
      */
-    getJSON('../data/earth-like-results.json')
-    .then(function(response) {
+		// getJSON('../data/earth-like-results.json').then(function(response) {
+		// 	addSearchHeader(response.query);
 
-      addSearchHeader(response.query);
-
-      response.results.map(function(url) {
-        getJSON(url).then(createPlanetThumb);
-      });
-    });
-  });
+		// 	response.results.map(function(url) {
+		// 		getJSON(url).then(createPlanetThumb);
+		// 	});
+		// });
+		getJSON('../data/earth-like-results.json').then().catch();
+	});
 })(document);
